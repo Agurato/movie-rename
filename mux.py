59687a05ded11759cedb14b5b2bfc0a9a0994bbc
Agurato <Agurato@users.noreply.github.com>
2021-11-12 10:32:22 +0100

    fix default tracks for subtitles

--- mux.py
+++ mux.py
@@ -15,33 +15,44 @@ class Multiplexer:
         self.movie_name = os.path.basename(movie_path)
         self.parent_dir = os.path.abspath(os.path.join(movie_path, os.pardir))
         self.ext_index = self.movie_name.rfind(".")
-        self.output_path = os.path.join(self.parent_dir, self.movie_name[:self.ext_index] + ".mux" + self.movie_name[self.ext_index:])
+        self.output_path = os.path.join(
+            self.parent_dir,
+            self.movie_name[: self.ext_index]
+            + ".mux"
+            + self.movie_name[self.ext_index :],
+        )
         self.subtitles = []
 
         self.failed = False
         self.msg = ""
 
     def mux(self, no_subtitles):
-        self.subtitles = get_subtitles(self.parent_dir, self.movie_name[:self.ext_index])
+        self.subtitles = get_subtitles(
+            self.parent_dir, self.movie_name[: self.ext_index]
+        )
 
         sub_params = f""
-        for sub in self.subtitles:
-            sub_filename = os.path.basename(sub)
-            sub_info = sub_filename[self.ext_index:sub_filename.rfind(".")][1:]
-            if 'forced' in sub_info:
+
+        for sub_lang, file in self.subtitles.items():
+            if "forced" in sub_lang:  # Don't handle forced subs
                 self.failed = True
                 self.msg = "Forced subtitle"
-            if len(sub_info) == 2:
-                sub_params += f" --language 0:{sub_info} ( \"{sub}\" )"
-            else:
-                sub_params += f" --language 0:en ( \"{sub}\" )"
+                return
+            if len(sub_lang) != 2:  # English as default language
+                sub_lang = "en"
+            sub_params += f" --language 0:{sub_lang}"
+            if (
+                sub_lang != "fr" and "fr" in self.subtitles
+            ):  # If non french sub or if there is a french sub
+                sub_params += f" --default-track 0:0"
+            sub_params += f' ( "{file}" )'
 
         track_order = ""
         if len(self.subtitles) == 0:
             self.failed = True
             self.msg = "No subtitle file"
         if len(self.subtitles) == 2:
-            track_order = " --track-order \"1:0,0:0\""
+            track_order = ' --track-order "1:0,0:0"'
         elif len(self.subtitles) > 2:
             self.failed = True
             self.msg = "More than 2 subtitles"
@@ -49,16 +60,20 @@ class Multiplexer:
         get_track_number(self.mkvtools_path, self.movie_path)
         track_names = ""
         for index in range(0, get_track_number(self.mkvtools_path, self.movie_path)):
-            track_names += f" --track-name {index}:\"\""
+            track_names += f' --track-name {index}:""'
 
         if not self.failed:
             nosub_param = ""
             if no_subtitles:
                 nosub_param = " --no-subtitles"
-            params = f"--output \"{self.output_path}\" {sub_params} --no-attachments {nosub_param} {track_names} ( \"{self.movie_path}\" ) --title \"{os.path.splitext(self.movie_name)[0]}\" {track_order}"
+            params = f'--output "{self.output_path}" {sub_params} --no-attachments {nosub_param} {track_names} ( "{self.movie_path}" ) --title "{os.path.splitext(self.movie_name)[0]}" {track_order}'
             print("Muxing " + self.movie_path)
             try:
-                subprocess.check_output(self.mkvtools_path + "\\mkvmerge.exe  " + params, stderr=subprocess.STDOUT, shell=True)
+                subprocess.check_output(
+                    self.mkvtools_path + "\\mkvmerge.exe  " + params,
+                    stderr=subprocess.STDOUT,
+                    shell=True,
+                )
             except subprocess.CalledProcessError as e:
                 self.failed = True
                 self.msg = str(e.output)
@@ -89,7 +104,7 @@ def get_movies(root_dir):
     for root, directories, filenames in os.walk(root_dir):
         for f in filenames:
             file_mime = mimetypes.guess_type(f)[0]
-            if file_mime is not None and 'video/' in file_mime:
+            if file_mime is not None and "video/" in file_mime:
                 movies.append(os.path.join(root, f))
 
     return movies
@@ -102,13 +117,21 @@ def get_subtitles(parent_path, movie_name):
     :param movie_name:
     :return:
     """
-    all_files = [f for f in os.listdir(parent_path) if os.path.isfile(os.path.join(parent_path, f))]
-    subtitles = []
+    all_files = [
+        f
+        for f in os.listdir(parent_path)
+        if os.path.isfile(os.path.join(parent_path, f))
+    ]
+    subtitles = {}
 
     for f in all_files:
         if movie_name in f:
             if f.endswith(".srt"):
-                subtitles.append(os.path.join(parent_path, f))
+                filename = os.path.basename(f)
+                sub_lang = filename[len(movie_name) : filename.rfind(".")][
+                    1:
+                ]  # get language
+                subtitles[sub_lang] = os.path.join(parent_path, f)
 
     return subtitles
 
@@ -120,7 +143,13 @@ def get_track_number(mkvtools_path, file):
     :param file:
     :return: number of tracks in file
     """
-    file_info = subprocess.Popen(f"{mkvtools_path}\\mkvinfo.exe \"{file}\"", shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8")
+    file_info = (
+        subprocess.Popen(
+            f'{mkvtools_path}\\mkvinfo.exe "{file}"', shell=True, stdout=subprocess.PIPE
+        )
+        .stdout.read()
+        .decode("utf-8")
+    )
     count_tracks = 0
 
     record_tracks = False
@@ -137,7 +166,7 @@ def get_track_number(mkvtools_path, file):
     return count_tracks
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     failed_files = []
     mkvtools_path = r"D:\Logiciels\mkvtoolnix"
